51b16a7cfa392d55aae18491a2faed2ad7d97869
Nilesh Verma <nileshverma19@gmail.com>
2021-06-10 22:00:18 +0530

    Heroku Update on files

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const port = process.env.PORT || 3000
+
 //using path default node module to set path variables
 const publicdirectorypath = path.join(__dirname,'../public')
 const viewpath = path.join(__dirname,'../public/templates/views')
@@ -114,6 +116,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000")
+app.listen(port,()=>{
+    console.log("Server is up on port "+port)
 })
